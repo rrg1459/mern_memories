159b8fa707596947da75a8c6fde503b5146039eb
rafaDev <rrg1459@hotmail.com>
2023-12-04 12:56:48 -0400

    Add GoogleOAuthProvider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,20 +2,25 @@ import React from 'react';
 import { Container } from '@material-ui/core';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import { Auth } from './components/Auth/Auth';
 
+console.log('xxx process.env-->: ', process.env);
+
 const App = () => (
-  <BrowserRouter>
-    <Container maxWidth="lg">
-      <Navbar />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/auth" exact component={Auth} />
-      </Switch>
-    </Container>
-  </BrowserRouter>
+  <GoogleOAuthProvider clientId='197370610713-p9ga128lfg6ur5f1h8otloc2lg95bgg5.apps.googleusercontent.com'>
+    <BrowserRouter>
+      <Container maxWidth="lg">
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/auth" exact component={Auth} />
+        </Switch>
+      </Container>
+    </BrowserRouter>
+  </GoogleOAuthProvider>
 );
 
 export default App;
